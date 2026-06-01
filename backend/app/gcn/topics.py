f6bedc9c4151edd52d024822ad53e1f94e776595
Maruf <maruffnadaf@gmail.com>
2026-06-01 12:16:29 +0530

    feat: configure GCN topics for listening

---
+++ backend/app/gcn/topics.py
@@ -0,0 +1,8 @@
+TOPICS = [
+    "gcn.notices.chime.frb",
+    "gcn.notices.einstein_probe.wxt.alert",
+    "gcn.notices.icecube.lvk_nu_track_search",
+    "gcn.notices.icecube.gold_bronze_track_alerts",
+    "igwn.gwalert",
+    "gcn.notices.swift.bat.guano"
+]
